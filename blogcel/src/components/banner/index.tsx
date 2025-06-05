7b8bbab3c1cf355475b692fab9f0545a2e320396
carloacafejs <carloscafe.js@gmail.com>
2025-06-05 01:38:09 -0300

    fix: bug no slide

--- blogcel/src/components/banner/index.tsx
+++ blogcel/src/components/banner/index.tsx
@@ -8,7 +8,7 @@ const Banner = () => {
     <div className="max-w-5xl w-full  rounded-b-2xl rounded-t-md mx-auto my-4 h-78 bg-gray-900 flex items-center justify-center">
       <h1 className="text-white text-2xl font-bold ">  </h1>
       <Splide
-        className="w-full h-full rounded-t-md"
+        className="w-full h-ful bg-amber-950 rounded-t-md"
         aria-label=" "
         options={{
           type: 'loop',
@@ -17,7 +17,7 @@ const Banner = () => {
           interval: 3000,
         }}
       >
-        <SplideSlide className="w-full h-50 bg-amber-300">Slide 01</SplideSlide>
+        <SplideSlide className="w-full h-full bg-amber-300">Slide 01</SplideSlide>
         <SplideSlide className="w-full h-50 bg-amber-400">Slide 02</SplideSlide>
         <SplideSlide className="w-full h-50 bg-amber-500">Slide 03</SplideSlide>
       </Splide>
